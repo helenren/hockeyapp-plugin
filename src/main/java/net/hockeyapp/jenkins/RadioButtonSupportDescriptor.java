152c8ee46054fa91deafd8adbae0877224de91f1
Qingchun Ren <qren@internal.salesforce.com>
2016-04-15 13:24:24 -0700

    create a new xamarin

--- src/main/java/net/hockeyapp/jenkins/RadioButtonSupportDescriptor.java
+++
@@ -1,8 +0,0 @@
-package net.hockeyapp.jenkins;
-
-import hudson.model.Descriptor;
-import hudson.model.Saveable;
-
-public abstract class RadioButtonSupportDescriptor<T extends RadioButtonSupport> extends Descriptor<RadioButtonSupport> implements Saveable {
-
-}
